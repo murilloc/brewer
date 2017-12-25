e372b9f18d0b29d6833b635cfcdaefd4868bae95
Murillo Cesar Pinto Nogueira <murilloc@gmail.com>
2017-12-25 21:36:33 -0200

    Thymeleaf Configurado

---
+++ brewer/src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -0,0 +1,33 @@
+package com.algaworks.brewer.model;
+
+public class Cerveja {
+
+	private String sku;
+	private String nome;
+	private String marca;
+	
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+}
