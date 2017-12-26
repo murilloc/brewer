8bd42d4f11e953150d3d2d268d30075dd0e184b6
Murillo Cesar Pinto Nogueira <murilloc@gmail.com>
2017-12-26 12:15:33 -0200

    3.8. Forward e Redirect

--- brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -5,9 +5,11 @@ package com.algaworks.brewer.controller;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
 
@@ -20,12 +22,14 @@ public class CervejasController {
 	}
 
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result) {
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		if (result.hasErrors()) {
-			System.out.println("Erro de validação");
+			model.addAttribute("mensagem","Erro de validação"); // Ao fazer o FORWARD atributo mensagem ficará disponível paro o Thymeleaf	
+			return "cerveja/CadastroCerveja"; // Utiliza HTTP Forward	
 		}
-		System.out.println(">>>>> Cadastrar nova Cerveja: " + cerveja.getSku() + "   " + cerveja.getNome());
-		return "cerveja/CadastroCerveja";
+				
+		attributes.addFlashAttribute("mensagem","Cerveja salva com sucesso"); // Ao fazer o REDIRECT atributo mensagem ficará disponível paro o Thymeleaf	
+		return "redirect:/cervejas/novo"; // Utiliza HTTP Redirect
 	}
 
 }
